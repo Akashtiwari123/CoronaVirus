9c8749228cbcb59a4816437af1ab8ad618cf050a
Akash Tiwari <drakashti@gmail.com>
2020-02-10 15:33:15 +0530

    Removed extra scripts

--- src/Home.js
+++ src/Home.js
@@ -68,8 +68,6 @@ class Home extends Component {
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           />
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" />
           <div style={{ marginLeft: "450px" }}>
             {" "}
@@ -88,7 +86,6 @@ class Home extends Component {
           >
             Logout
           </button>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
 
         <main>
--- src/Login.js
+++ src/Login.js
@@ -35,8 +35,6 @@ class Login extends Component {
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           />
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" />
 
           <div className="head">
