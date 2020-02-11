02cd0e84186a53986160303a7bd64828b2c3e872
Akash Tiwari <drakashti@gmail.com>
2020-02-11 10:26:40 +0530

    Api Data and Home page css changed

--- src/Home.js
+++ src/Home.js
@@ -73,112 +73,152 @@ class Home extends Component {
     if (!this.state.api && !this.state.load) {
       homepage = (
         <div>
-          <p className="para">
-            {" "}
-            Coronaviruses (CoV) are a large family of viruses that cause illness
-            ranging from the common cold to more severe diseases such as Middle
-            East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory
-            Syndrome (SARS-CoV). A novel coronavirus (nCoV) is a new strain that
-            has not been previously identified in humans.
-          </p>
-          <p>
-            Coronaviruses are zoonotic, meaning they are transmitted between
-            animals and people. Detailed investigations found that SARS-CoV was
-            transmitted from civet cats to humans and MERS-CoV from dromedary
-            camels to humans. Several known coronaviruses are circulating in
-            animals that have not yet infected humans.
-          </p>{" "}
-          <p>
-            Common signs of infection include respiratory symptoms, fever,
-            cough, shortness of breath and breathing difficulties. In more
-            severe cases, infection can cause pneumonia, severe acute
-            respiratory syndrome, kidney failure and even death.
-          </p>
-          <h1 className="p">
-            Basic protective measures against the new coronavirus
-          </h1>
-          <h3 className="p">Wash your hands frequently</h3>
-          <p>
-            Wash your hands frequently with soap and water or use an
-            alcohol-based hand rub if your hands are not visibly dirty.
-          </p>
-          <p>
-            <strong>Why?</strong>
-            Washing your hands with soap and water or using alcohol-based hand
-            rub eliminates the virus if it is on your hands.&nbsp;
-          </p>
-          <h3 className="p">Practice respiratory hygiene</h3>
-          <p>
-            When coughing and sneezing, cover mouth and nose with flexed elbow
-            or tissue – discard tissue immediately into a closed bin and clean
-            your hands with alcohol-based hand rub or soap and water.
-          </p>
-          <p>
-            Why? Covering your mouth and nose when coughing and sneezing prevent
-            the spread of germs and viruses. If you sneeze or cough into your
-            hands, you may contaminate objects or people that you touch.
-          </p>
-          <h3 className="p">Maintain social distancing</h3>
-          <p>
-            Maintain at least 1 metre (3 feet) distance between yourself and
-            other people, particularly those who are coughing, sneezing and have
-            a fever.
-          </p>
-          <p>
-            <strong>Why?</strong> When someone who is infected with a
-            respiratory disease, like 2019-nCoV, coughs or sneezes they project
-            small droplets containing the virus. If you are too close, you can
-            breathe in the virus.
-          </p>
-          <p>
-            <iframe
-              width="560"
-              height="475"
-              src="https://www.youtube.com/embed/6Ooz1GZsQ70"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen=""
-            >
-              &nbsp;
-            </iframe>
-          </p>
-          <h3 className="p">Avoid touching eyes, nose and mouth</h3>
-          <p>
-            <strong>Why?&nbsp;</strong>
-            Hands touch many surfaces which can be contaminated with the virus.
-            If you touch your eyes, nose or mouth with your contaminated hands,
-            you can transfer the virus from the surface to yourself.&nbsp;
-          </p>
-          <h3 className="p">
-            If you have fever, cough and difficulty breathing, seek medical care
-            early
-          </h3>
-          <p>
-            Tell your health care provider if you have traveled in an area in
-            China where 2019-nCoV has been reported, or if you have been in
-            close contact with someone with who has traveled from China and has
-            respiratory symptoms.
-          </p>
-          <p>
-            <strong>Why?</strong> Whenever you have fever, cough and difficulty
-            breathing it’s important to seek medical attention promptly as this
-            may be due to a respiratory infection or other serious condition.
-            Respiratory symptoms with fever can have a range of causes, and
-            depending on your personal travel history and circumstances,
-            2019-nCoV could be one of them.
-          </p>
-          <p>
-            <iframe
-              width="560"
-              height="475"
-              src="https://www.youtube.com/embed/qF42gZVm1Bo"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen=""
-            >
-              &nbsp;
-            </iframe>
-          </p>
+          <div className="para">
+            <p>
+              {" "}
+              <strong>Coronaviruses (CoV) </strong> are a large family of
+              viruses that cause illness ranging from the common cold to more
+              severe diseases such as{" "}
+              <a
+                href="https://www.who.int/emergencies/mers-cov/en/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Middle East Respiratory Syndrome (MERS-CoV)
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://www.who.int/csr/sars/en/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Severe Acute Respiratory Syndrome (SARS-CoV)
+              </a>
+              .
+              <a
+                href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                A novel coronavirus (nCoV){" "}
+              </a>
+              is a new strain that has not been previously identified in humans.
+            </p>
+            <p>
+              Coronaviruses are zoonotic, meaning they are transmitted between
+              animals and people. Detailed investigations found that SARS-CoV
+              was transmitted from civet cats to humans and MERS-CoV from
+              dromedary camels to humans. Several known coronaviruses are
+              circulating in animals that have not yet infected humans.
+            </p>{" "}
+            <p>
+              Common signs of infection include respiratory symptoms, fever,
+              cough, shortness of breath and breathing difficulties. In more
+              severe cases, infection can cause pneumonia, severe acute
+              respiratory syndrome, kidney failure and even death.
+            </p>
+          </div>
+          <div className="content">
+            <h1 className="p" style={{ fontSize: "30px", fontStyle: "bold" }}>
+              Basic protective measures against the new coronavirus
+            </h1>
+            <hr />
+            <p>
+              <iframe
+                title="corona-video1"
+                width="560"
+                height="475"
+                src="https://www.youtube.com/embed/6Ooz1GZsQ70"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""
+                className="frame"
+              >
+                &nbsp;
+              </iframe>
+            </p>
+            <hr />
+            <h3 className="p">Wash your hands frequently</h3>
+            <p>
+              Wash your hands frequently with soap and water or use an
+              alcohol-based hand rub if your hands are not visibly dirty.
+            </p>
+            <p>
+              <strong>Why?</strong> Washing your hands with soap and water or
+              using alcohol-based hand rub eliminates the virus if it is on your
+              hands.&nbsp;
+            </p>
+            <hr />
+            <h3 className="p">Practice respiratory hygiene</h3>
+            <p>
+              When coughing and sneezing, cover mouth and nose with flexed elbow
+              or tissue – discard tissue immediately into a closed bin and clean
+              your hands with alcohol-based hand rub or soap and water.
+            </p>
+            <p>
+              <strong>Why?</strong> Covering your mouth and nose when coughing
+              and sneezing prevent the spread of germs and viruses. If you
+              sneeze or cough into your hands, you may contaminate objects or
+              people that you touch.
+            </p>
+            <hr />
+            <h3 className="p">Maintain social distancing</h3>
+            <p>
+              Maintain at least 1 metre (3 feet) distance between yourself and
+              other people, particularly those who are coughing, sneezing and
+              have a fever.
+            </p>
+            <p>
+              <strong>Why?</strong> When someone who is infected with a
+              respiratory disease, like 2019-nCoV, coughs or sneezes they
+              project small droplets containing the virus. If you are too close,
+              you can breathe in the virus.
+            </p>
+            <hr />
+            <h3 className="p">Avoid touching eyes, nose and mouth</h3>
+            <p>
+              <strong>Why?&nbsp;</strong>
+              Hands touch many surfaces which can be contaminated with the
+              virus. If you touch your eyes, nose or mouth with your
+              contaminated hands, you can transfer the virus from the surface to
+              yourself.&nbsp;
+            </p>
+            <hr />
+            <h3 className="p">
+              If you have fever, cough and difficulty breathing, seek medical
+              care early
+            </h3>
+            <p>
+              Tell your health care provider if you have traveled in an area in
+              China where 2019-nCoV has been reported, or if you have been in
+              close contact with someone with who has traveled from China and
+              has respiratory symptoms.
+            </p>
+            <p>
+              <strong>Why?</strong> Whenever you have fever, cough and
+              difficulty breathing it’s important to seek medical attention
+              promptly as this may be due to a respiratory infection or other
+              serious condition. Respiratory symptoms with fever can have a
+              range of causes, and depending on your personal travel history and
+              circumstances, 2019-nCoV could be one of them.
+            </p>
+            <hr />
+            <p>
+              <iframe
+                title="corona-video2"
+                width="560"
+                height="475"
+                src="https://www.youtube.com/embed/qF42gZVm1Bo"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""
+                className="frame"
+              >
+                &nbsp;
+              </iframe>
+            </p>
+          </div>
         </div>
       );
     }
@@ -186,10 +226,10 @@ class Home extends Component {
     if (this.state.api) {
       apiList = (
         <div>
-          <div className="list">
+          <div className="lists">
             <font style={{ marginRight: "1000px", fontSize: "20px" }}>
               <img src={listIcon} alt="Listicon" />
-              CoronaVirus API Data Samples{" "}
+              CoronaVirus API Data{" "}
             </font>
           </div>
           {list}
@@ -351,17 +391,18 @@ class Home extends Component {
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" />
-          <div style={{ marginLeft: "450px" }}>
+          <button className="hbtn" onClick={this.onHandleForm}>
+            Registration
+          </button>
+          <button className="hbtn" onClick={this.onHandleApi}>
+            Data
+          </button>
+          <div style={{ marginLeft: "250px" }}>
             {" "}
             <img src={logo} alt="logo" />
             <font>CoronaVirus </font>{" "}
           </div>
-          <button className="btn btn-primary" onClick={this.onHandleForm}>
-            Registration
-          </button>
-          <button className="btn btn-primary" onClick={this.onHandleApi}>
-            Data Samples
-          </button>
+
           <font style={{ marginLeft: "210px", marginTop: "30px" }}>
             Welcome, {this.props.location.state.user}
           </font>
